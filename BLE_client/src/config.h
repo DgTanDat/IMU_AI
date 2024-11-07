289b0c9800bd987b818e015b793a3a5a63544fd3
DgTanDat <21521927@gm.uit.edu.vn>
2024-11-07 21:00:41 +0700

    update car code - rtos - cal_distance funct v.v

--- BLE_client/src/config.h
+++ BLE_client/src/config.h
@@ -2,25 +2,69 @@
 //BLE Server name (the other ESP32 name running the server sketch)
 #define bleServerName "Movella DOT"
 #define DATA_LENGTH 4
-float delta_t = 0.1;
+#define STABLE_THREADHOLD 8
+#define NONE -1
+
+float f = 60;
+float delta_t = 1/f;
 static int count = 0;
+
+
+const int packageCounter = 3*f;
+
 union IntFloat {
     unsigned int i;
     float f;
 };
 
-const float threadhold = 0.06;
-const float threadhold1 = 0.045;
+struct NotifyData {
+  uint8_t pData[32]; // Thay đổi kích thước phù hợp với length của notify
+  size_t length;
+};
 
-static float positionX = 0;
-static float positionY = 0;
-static float positionZ = 0;
+enum{
+  STOP,
+  GOSTRAIGHT,
+  TURNRIGHT,
+};
 
-float accX = 0;
-float accY = 0;
-float accZ = 0;
+const float threadhold = 0.05;
 
-static float curVecX = 0;
-static float curVecY = 0;
-static float curVecZ = 0;
+float positionX = 0;
+float positionY = 0;
+// static float positionZ = 0;
 
+float startPosX = 0;
+float startPosY = 0;
+
+float curFaccX = 0;
+float curFaccY = 0;
+// float curFaccZ = 0;
+float lastFaccX = 0;
+float lastFaccY = 0;
+// float lastFaccZ = 0;
+
+int counterX = 0;
+int counterY = 0;
+// int counterZ = 0;
+
+float curVelX = 0;
+float curVelY = 0;
+// float curVelZ = 0;
+float lastVelX = 0;
+float lastVelY = 0;
+// float lastVelZ = 0;
+
+int initLastState = GOSTRAIGHT;
+int initState = GOSTRAIGHT;
+
+float focusYaw = 0;
+float curYaw = 0;
+float lastYaw = 0;
+
+bool isWrite = false;
+bool haveInitYaw = false;
+float angle_thread_high;
+float angle_thread_low;
+
+int waitTime = 0;
--- BLE_client/src/main.cpp
+++ BLE_client/src/main.cpp
@@ -2,9 +2,17 @@
 #include "BLEDevice.h"
 #include "config.h"
 #include "MotorDriver.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
 #include "math.h"
 
+QueueHandle_t notifyQueue;
+QueueHandle_t stateQueue;
+QueueHandle_t lastStateQueue;
+
 MotorDriver m;
+NotifyData notifyDatacb;
 
 /* UUID's of the service, characteristic that we want to read*/
 // BLE Service
@@ -23,7 +31,6 @@ static BLEUUID shortPayLoadCharacteristicUUID("15172004-4947-11E9-8646-D663BD873
 //Flags stating if should begin connecting and if the connection is up
 static boolean doConnect = false;
 static boolean connected = false;
-static boolean doScan= false;
 
 //Address of the peripheral device. Address will be found during scanning...
 static BLEAddress *pServerAddress;
@@ -31,6 +38,7 @@ static BLEAddress *pServerAddress;
 //Characteristicd that we want to read
 static BLERemoteCharacteristic* ctrlCharacteristic;
 static BLERemoteCharacteristic* medPayloadCharacteristic;
+// static BLERemoteCharacteristic* deviceCtrlCharacteristic;
 
 void turnLeft(int speed){
   m.motor(1, BACKWARD, speed);
@@ -74,19 +82,19 @@ void release(){
   m.motor(4, RELEASE, 255);
 }
 
-void timeStampConvert(uint8_t* data, int startIndex){
-  uint32_t timeStamp = 0;
-  for(int i=startIndex; i<DATA_LENGTH; i++){
-    timeStamp |= data[i] << ((i) * 8);
-  }
-  Serial.print("TimeStamp is:");
-  Serial.println(timeStamp);
-}
+// void timeStampConvert(uint8_t* data, int startIndex){
+//   uint32_t timeStamp = 0;
+//   for(int i=startIndex; i<DATA_LENGTH; i++){
+//     timeStamp |= data[i] << ((i) * 8);
+//   }
+//   Serial.print("TimeStamp is:");
+//   Serial.println(timeStamp);
+// }
 
-float velocityCal(float freeAcc, float deltaTime){
-  if(freeAcc < 0.05 && freeAcc > -0.05) return 0;
-  else return 0.5*(freeAcc * deltaTime * deltaTime);
-}
+// float velocityCal(float freeAcc, float deltaTime){
+//   if(freeAcc < 0.05 && freeAcc > -0.05) return 0;
+//   else return 0.5*(freeAcc * deltaTime * deltaTime);
+// }
 
 float dataConvert(uint8_t* data, int startIndex){
   union IntFloat temp;
@@ -96,20 +104,9 @@ float dataConvert(uint8_t* data, int startIndex){
     temp.i |= data[i] << ((count) * 8);
     count++;
   }
-  Serial.println(temp.f);
+  // Serial.println(temp.f);
   return temp.f;
 }
-
-static float focusYaw = 0;
-static float yaw = 0;
-bool calibFlag = false;
-
-bool isWrite = false;
-bool isForward = true;
-bool isTurn = false;
-bool haveInitYaw = false;
-float angle_thread_high;
-float angle_thread_low;
 
 float cal_angle(float angle, float angular_turn){
   float gap = 0;
@@ -126,89 +123,29 @@ float cal_angle(float angle, float angular_turn){
   }
 }
 
+float cal_distance(float x0, float y0, float x1, float y1){
+  return sqrt((x0-x1)*(x0-x1) + (y0-y1)*(y0-y1));
+}
+
 //When the BLE Server sends a new temperature reading with the notify property
 static void NotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
                                         uint8_t* pData, size_t length, bool isNotify) {
   if(pBLERemoteCharacteristic->getUUID().toString() == medPayLoadCharacteristicUUID.toString()) 
   {
-      Serial.print("Characteristic (Notify) from server: ");
-      for(int i = 0; i<length; i++) {
-        Serial.print(pData[i]);
-        Serial.print('.');
+      // Serial.print("Characteristic (Notify) from server: ");
+      // for(int i = 0; i<length; i++) {
+      //   Serial.print(pData[i]);
+      //   Serial.print('.');
+      // }
+      // Serial.println();  
+
+      if(count >= packageCounter){
+        memcpy(notifyDatacb.pData, pData, length);
+        notifyDatacb.length = length;
+
+        xQueueSend(notifyQueue, &notifyDatacb, portMAX_DELAY);
       }
-      Serial.println();  
-
-      timeStampConvert(pData, 0);
-      Serial.print("x_euler_e:");
-      dataConvert(pData, 4);
-      Serial.print("y_euler_e:");
-      dataConvert(pData, 8);
-      Serial.print("z_euler_e:");
-      yaw = dataConvert(pData, 12);
-      Serial.print("x acc:");
-      accX = dataConvert(pData, 16);
-      Serial.print("y acc:");
-      accY = dataConvert(pData, 20);
-      Serial.print("z acc:");
-      accZ = dataConvert(pData, 24);
-
-      accZ = accZ - 0.33;
-      if(!calibFlag){
-        if((fabs(accX) <= threadhold1) && (fabs(accY) <= threadhold1) && (fabs(accZ) <= threadhold1)) calibFlag = true;
-      }
-      
-      if(calibFlag){
-        if(!haveInitYaw){
-          focusYaw = yaw;
-          haveInitYaw = true;
-          angle_thread_high = cal_angle(focusYaw, -88.5);
-          angle_thread_low = cal_angle(focusYaw, -91.5);
-        }
-        if(!isForward){
-          if(!isTurn){
-            turnRight(180);
-            isTurn = true;
-          }
-        }
-        
-        if((angle_thread_low <= yaw) && (yaw <= angle_thread_high)){
-          brake();
-          isForward = true;
-          isTurn = false;
-          focusYaw = yaw;
-          angle_thread_high = cal_angle(focusYaw, -89);
-          angle_thread_low = cal_angle(focusYaw, -91); 
-        }
-
-        if(fabs(accX) <= threadhold){
-          accX = 0;
-          curVecX = 0;
-        }
-        if(fabs(accY) <= threadhold){
-          accY = 0;
-          curVecY = 0;
-        } 
-        if(fabs(accZ) <= threadhold){
-          accZ = 0;
-          curVecZ = 0;
-        } 
-
-        // Serial.println(accX);
-        // Serial.println(accY);
-        // Serial.println(accZ);
-        // curVecX += accX*delta_t;
-        // curVecY += accY*delta_t;
-        // curVecZ += accZ*delta_t;
-        // positionX += delta_t*curVecX;
-        // Serial.print("x position: ");
-        // Serial.println(positionX);
-        // positionY += delta_t*curVecY;
-        // Serial.print("y position: ");
-        // Serial.println(positionY);
-        // positionZ += delta_t*curVecZ;
-        // Serial.print("z position: ");
-        // Serial.println(positionZ);
-      }
+      else count = count + 1;
   }
 }
 
@@ -239,25 +176,36 @@ bool connectToServer(BLEAddress pAddress) {
   Serial.println(" - Connected to server");
  
   // Obtain a reference to the service we are after in the remote BLE server.
-  BLERemoteService* pRemoteService = pClient->getService(measurementServiceUUID);
-  if (pRemoteService == nullptr) {
-    Serial.print("Failed to find our service UUID: ");
+  // BLERemoteService* pRemoteService0 = pClient->getService(configServiceUUID);
+  // if (pRemoteService0 == nullptr) {
+  //   Serial.print("Failed to find configuration service UUID: ");
+  //   Serial.println(configServiceUUID.toString().c_str());
+  //   return (false);
+  // }
+
+  BLERemoteService* pRemoteService1 = pClient->getService(measurementServiceUUID);
+  if (pRemoteService1 == nullptr) {
+    Serial.print("Failed to find measurement service UUID: ");
     Serial.println(measurementServiceUUID.toString().c_str());
     return (false);
   }
  
   // Obtain a reference to the characteristics in the service of the remote BLE server.
-  ctrlCharacteristic = pRemoteService->getCharacteristic(measureCtrlCharacteristicUUID);
-  medPayloadCharacteristic = pRemoteService->getCharacteristic(medPayLoadCharacteristicUUID);
+  // deviceCtrlCharacteristic = pRemoteService0->getCharacteristic(devicectrlCharacteristicUUID);
+  ctrlCharacteristic = pRemoteService1->getCharacteristic(measureCtrlCharacteristicUUID);
+  medPayloadCharacteristic = pRemoteService1->getCharacteristic(medPayLoadCharacteristicUUID);
 
 
   if (ctrlCharacteristic == nullptr || medPayloadCharacteristic == nullptr) {
-    Serial.print("Failed to find our characteristic UUID");
+    Serial.print("Failed to find our characteristics UUID");
     return false;
   }
-  if(connectCharacteristic(pRemoteService, ctrlCharacteristic) == false || connectCharacteristic(pRemoteService, medPayloadCharacteristic) == false)
+
+  if(connectCharacteristic(pRemoteService1, ctrlCharacteristic) == false || 
+  connectCharacteristic(pRemoteService1, medPayloadCharacteristic) == false)
     connected = false;
-  Serial.println(" - Found our characteristics");
+  Serial.println(" - Found measurement characteristics");
+  
   return true;
 }
 
@@ -275,81 +223,181 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   }
 };
  
+void processNotifyTask(void *parameter) {
+  NotifyData notifyData;
+  int statePN = GOSTRAIGHT;
+  int lastStatePN;
+  while (true) {
+    if ((xQueueReceive(notifyQueue, &notifyData, portMAX_DELAY) == pdPASS)
+    &&(xQueueReceive(lastStateQueue, &lastStatePN, portMAX_DELAY) == pdPASS)) {
+      
+      curYaw = dataConvert(notifyData.pData, 12);
+      curFaccX = dataConvert(notifyData.pData, 16);
+      curFaccY = dataConvert(notifyData.pData, 20);
 
+      if(!haveInitYaw){
+        focusYaw = curYaw;
+        haveInitYaw = true;
+        angle_thread_high = cal_angle(focusYaw, -89);
+        angle_thread_low = cal_angle(focusYaw, -91);
+      }
+      switch (lastStatePN)
+      {
+        case STOP:
+          waitTime -= 1;
+          if(waitTime <= 0){
+            statePN = GOSTRAIGHT;
+          }
+          else{
+            statePN = STOP;
+          }
+          break;
+        case GOSTRAIGHT:
+          if((-threadhold <= curFaccX)&&(curFaccX <= threadhold)){
+            curFaccX = 0;
+            counterX++;
+          }
+          else{
+            counterX = 0;
+          }
+          if((-threadhold <= curFaccY)&&(curFaccY <= threadhold)){
+            curFaccY = 0;
+            counterY++;
+          }
+          else{
+            counterY = 0;
+          }  
+          if(counterX >= STABLE_THREADHOLD){
+            curVelX = 0;
+          }
+          if(counterY >= STABLE_THREADHOLD){
+            curVelY = 0;
+          }
+
+          curVelX += 0.5*(lastFaccX + curFaccX)*delta_t;
+          curVelY += 0.5*(lastFaccY + curFaccY)*delta_t;
+          
+          positionX += 0.5*(lastVelX + curVelX)*delta_t;
+          positionY += 0.5*(lastVelY + curVelY)*delta_t;
+
+          if(cal_distance(startPosX, startPosY, positionX, positionY) >= 0.7){
+            brake();
+            statePN = TURNRIGHT;
+            startPosX = positionX;
+            startPosY = positionY;
+          }
+          else{
+            statePN = GOSTRAIGHT;
+          }
+          break;
+        case TURNRIGHT:
+          if((angle_thread_low <= curYaw) && (curYaw <= angle_thread_high)){
+            focusYaw = curYaw;
+            angle_thread_high = cal_angle(focusYaw, -89);
+            angle_thread_low = cal_angle(focusYaw, -91); 
+            statePN = STOP; 
+            positionX = 0;
+            waitTime = 990;
+          }
+          else{
+            statePN = TURNRIGHT;
+          }
+          
+          break;
+        default:
+          statePN = GOSTRAIGHT;
+          break;
+      }
+      xQueueSend(stateQueue, &statePN, portMAX_DELAY);
+      lastYaw = curYaw;
+      lastFaccX = curFaccX;
+      lastFaccY = curFaccY;
+      lastVelX = curVelX;
+      lastVelY = curVelY;
+    }
+  }
+}
+
+void startXsenMeasurementTask(void *parameter) {
+  while (true)
+  {
+    std::uint8_t txValue[3];
+
+    txValue[0] = 1;
+    txValue[1] = 1;
+    txValue[2] = 16;
+    if (doConnect == true) {
+      if (connectToServer(*pServerAddress)) {
+        Serial.println("We are now connected to the BLE Server.");
+
+        connected = true;
+        if(!isWrite){
+          ctrlCharacteristic->writeValue(txValue, 3);
+          isWrite = true;
+        }
+      } else {
+        Serial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
+      }
+      doConnect = false;
+    }
+    delay(1000);
+  }
+}
+
+void stateProcessTask(void *parameter) {
+  int stateSP = NONE;
+  int lastStateSP = NONE;
+  while (true)
+  {
+    if (xQueueReceive(stateQueue, &stateSP, portMAX_DELAY) == pdPASS) {
+      if(lastStateSP != stateSP){
+        switch (stateSP)
+        {
+          case STOP:
+            brake();
+            break;
+          case GOSTRAIGHT:
+            forward(250);
+            break;
+          case TURNRIGHT:
+            turnRight(80);
+            break;
+          default:
+            stateSP = STOP;
+            brake();
+            break;
+        }
+        lastStateSP = stateSP;
+      }
+      xQueueSend(lastStateQueue, &lastStateSP, portMAX_DELAY);
+    }
+  }
+}
 
 void setup() {
   
-  //Start serial communication
   Serial.begin(115200);
   Serial.println("Starting Arduino BLE Client application...");
-
 
   //Init BLE device
   BLEDevice::init("");
  
-  // // Retrieve a Scanner and set the callback we want to use to be informed when we
-  // // have detected a new device.  Specify that we want active scanning and start the
-  // // scan to run for 30 seconds.
   BLEScan* pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
   pBLEScan->start(30);
-  // forward(255);
-  // delay(2100);
-  // brake();
-  // delay(1000);
-  // turnRight(255);
-  // delay(800);
-  // brake();
+
+  notifyQueue = xQueueCreate(10, sizeof(NotifyData)); 
+  stateQueue = xQueueCreate(10, sizeof(int));
+  lastStateQueue = xQueueCreate(10, sizeof(int));
+
+  xQueueSend(lastStateQueue, &initLastState, portMAX_DELAY);
+
+  xTaskCreate(startXsenMeasurementTask, "startXsenMeasurementTask", 4000, NULL, 1, NULL);
+  xTaskCreate(processNotifyTask, "ProcessNotifyTask", 6000, NULL, 2, NULL);
+  xTaskCreate(stateProcessTask, "stateProcessTask", 6000, NULL, 2, NULL);
 }
 
 
 void loop() {
-  // If the flag "doConnect" is true then we have scanned for and found the desired
-  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
-  // connected we set the connected flag to be true.
-  std::uint8_t txValue[3];
-  txValue[0] = 1;
-  txValue[1] = 1;
-  txValue[2] = 16;
-  if (doConnect == true) {
-    if (connectToServer(*pServerAddress)) {
-      Serial.println("We are now connected to the BLE Server.");
-      
-      connected = true;
-      if(!isWrite){
-        ctrlCharacteristic->writeValue(txValue, 3);
-        isWrite = true;
-      }
-    } else {
-      Serial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
-    }
-    doConnect = false;
-  }
-
- 
-  if(isForward){
-    forward(255);
-    delay(2100);
-    brake();
-    delay(1000);
-    isForward = false;
-  }
-
-  
-  // if(count < 3){
-  //   forward(255);
-  // } else if(count < 6){
-  //   backward(255);
-  // } else if(count < 9){
-  //   turnLeft(255);
-  // } else if(count < 12){
-  //   turnRight(255);
-  // } else if(count < 15){
-  //   brake();
-  // } else{
-  //   count = 0;
-  // }
-  // count++;
-  delay(1000); // Delay a second between loops.
 }
